c374774312af0654608107009a67338f3c98dd78
technicalusers <arpithadayananda@outlook.com>
2018-07-31 14:32:17 +0530

    login page implemented

--- MyProject/resources/views/login.blade.php
+++ MyProject/resources/views/login.blade.php
@@ -3,18 +3,32 @@
 @section('content')
 
 
+
 <section>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-md-12">
-        <h1>Login Page</h1>
+  <div class="row">
+    <div class="col-md-7">
+      <div  class="bg-img">
+  
       </div>
     </div>
+    <div class="col-md-5">
+      <form action="/action_page.php">
+        <div class="form-block">
+          <p align="center"><b>Sign In</p><br>
+
+          <label for="email"><b>Email</b></label>
+          <input type="text" placeholder="Enter Email" name="email" required>
+
+          <label for="psw"><b>Password</b></label>
+          <input type="password" placeholder="Enter Password" name="psw" required>
+
+          <button type="submit" class="btn">Login</button><br><br>
+        </div>
+      </form>
   </div>
+
+  </div>
+
 </section>
-<style>
-  nav{
-    display: none !important;
-  }
-</style>
+
 @endsection
